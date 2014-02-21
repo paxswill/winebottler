3c14779a787c7c3ef516972c18531d4333e55977
Mike Kronenberg <mike.kronenberg@kronenberg.org>
2014-02-22 00:39:20 +0100

    Bugfixes, Retina Ions, Pushed Version to 1.7.13

---
+++ WineBottler/INWindowButton.h
@@ -0,0 +1,13 @@
+//
+//  INWindowButton.h
+//  WineBottler
+//
+//  Created by Mike Kronenberg on 03.12.13.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface INWindowButton : NSClipView
+
+@end
---
+++ WineBottler/INWindowButton.m
@@ -0,0 +1,29 @@
+//
+//  INWindowButton.m
+//  WineBottler
+//
+//  Created by Mike Kronenberg on 03.12.13.
+//
+//
+
+#import "INWindowButton.h"
+
+@implementation INWindowButton
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
